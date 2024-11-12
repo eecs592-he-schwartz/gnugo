7a989af42b1309cf97c221871fb15051022c313a
Johnson He <hejohns@umich.edu>
2024-11-12 04:50:44 +0000

    gnugo parameters for our evaluations

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+gnugo --mode gtp --boardsize 19 --quiet --komi 6.5 --score aftermath --capture-all-dead --chinese-rules --simple-ko --forbid-suicide --cache-size 128 --resign-allowed -o "$1"
