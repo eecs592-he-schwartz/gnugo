55ac663efa12a5abd72e91bca5e0e705dcd4db2c
Johnson He <hejohns@umich.edu>
2024-11-12 06:32:57 +0000

    more memory

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-gnugo --mode gtp --boardsize 19 --quiet --komi 6.5 --score aftermath --capture-all-dead --chinese-rules --simple-ko --forbid-suicide --cache-size 128 --resign-allowed
+gnugo --mode gtp --boardsize 19 --quiet --komi 6.5 --score aftermath --capture-all-dead --chinese-rules --simple-ko --forbid-suicide --cache-size 256 --resign-allowed
